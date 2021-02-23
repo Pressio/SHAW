01f8c815af974bbbc974d95f0c935b046c2dface
Francesco Rizzi <fnrizzi@sandia.gov>
2021-02-23 15:52:38 +0100

    update

--- docs/pelicanconf.py
+++ docs/pelicanconf.py
@@ -9,7 +9,7 @@ M_SITE_LOGO_TEXT = 'SHWAV'
 
 SITENAME = 'ShWav'
 SITESUBTITLE = 'Elastic Shear Waves'
-SITEURL = '.' 
+SITEURL = '' 
 # https://github.com/fnrizzi/SHAW/docs/output'
 
 # M_BLOG_NAME = ''
